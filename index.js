6b136202525c010c45a211e1951f36f3789680be
Kyle Holmberg <kylemh.email@gmail.com>
2019-02-24 17:07:59 -0800

    Create deduped_leads file from code

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-//
+const fs = require('fs');
+const leads = require('./leads.json');
+
+const dedupedLeads = leads;
+
+const jsonDedupedLeads = JSON.stringify(dedupedLeads, null, 2).concat('\n');
+
+fs.writeFileSync('deduped_leads.json', jsonDedupedLeads);

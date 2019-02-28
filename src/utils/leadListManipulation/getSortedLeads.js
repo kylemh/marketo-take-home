e232919ffe58177123f5cf5d325e62bb4a0ddfab
Kyle Holmberg <kylemh.email@gmail.com>
2019-02-28 12:14:01 -0800

    Add comment

--- src/utils/leadListManipulation/getSortedLeads.js
+++ src/utils/leadListManipulation/getSortedLeads.js
@@ -6,6 +6,9 @@
  * @returns {[Object.<string, any>]} sorted leads
  */
 function getSortedLeads(leads) {
+  // If stable sort is an issue, we can use Lodash's orderBy
+  // see: https://stackoverflow.com/questions/3026281/what-is-the-stability-of-the-array-sort-method-in-different-browsers
+  // for concern
   return [...leads].sort((a, b) => new Date(b.entryDate) - new Date(a.entryDate));
 }
 
